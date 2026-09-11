d76ffa585f362cb708e4abade160297aef26a623
Mantej Sokhi <sokhimantej99@gmail.com>
2026-09-11 16:44:13 -0700

    Added timing for vs_engine

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include<utility>
 #include<string>
 #include<sstream>
+#include<chrono>
+#include<random>
 
 #include"Vector.hpp"
 #include"Similarity.hpp"
@@ -17,11 +19,25 @@ int main(){
     std::string file_Name="/home/msokhi/Desktop/vs_engine/data/vectors.txt";
     vectorDB db=construct_DB(file_Name);
     */
-
-    vectorDB db=bm_file(3,50);
+    std::size_t dim=1000;
+    std::size_t num_Vec=1000;
+    
+    vectorDB db=bm_file(dim,num_Vec);
     db.print_db();
 
-    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<float> distrib(0,1);
+
+    std::vector<double> tempQ;
+    for(auto i=0;i<dim;++i){
+        tempQ.push_back(distrib(gen));
+    }
+
+    Vector user_Q(tempQ);
+
+    /*
     std::string user_Input;
     std::getline(std::cin,user_Input);
 
@@ -37,13 +53,23 @@ int main(){
     }
 
     Vector user_VV(user_V);
+    */
     
-    std::vector<std::pair<double,std::size_t>> res=knn_Search(user_VV,db,2);
+    std::vector<std::pair<double,std::size_t>> res;
+    
+    auto start=std::chrono::high_resolution_clock::now();
+    for(auto i=0;i<10;++i){
+        std::vector<std::pair<double,std::size_t>> res=knn_Search(user_Q,db,2);
+    }
+    auto end=std::chrono::high_resolution_clock::now();
 
+    auto duration=std::chrono::duration_cast<std::chrono::microseconds>((end-start)/10);
+    
     for(auto i=0;i<res.size();++i){
         std::cout<<res[i].first<<" "<<res[i].second<<'\n';
     }
-    
+    std::cout<<'\n';
+    std::cout<<"TIME -> "<<duration.count()<<" microseconds.\n";
     
     return 0;
 }
